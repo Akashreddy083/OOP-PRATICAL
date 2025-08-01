7f2684b7b999a5816f4adcf8c7578dae8d170a53
Akashreddy083 <2420080083@klh.edu.in>
2025-08-01 11:31:44 +0530

    Add files via upload

---
+++ eclipse-workspace/skilll/src/skilll/string.java
@@ -0,0 +1,21 @@
+package skilll;
+
+public class string {
+
+	public static void main(java.lang.String[] args) {
+	    int a;
+	    byte b;
+	    b = 120;
+	    a = b;
+	    System.out.println("a = " + a);
+	    System.out.println("b =" + b);
+	    long p;
+	    byte y;
+	    p = 12500;
+	    y = (byte)p;
+	    System.out.println("p =" + p);
+	    System.out.println("y =" + y);
+
+	  }
+
+	}
